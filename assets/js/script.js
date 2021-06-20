94d630e0342020e86e54d6938a3d5318be11614a
Maxwell Dunn <max.dunn63@gmail.com>
2021-06-20 14:29:33 -0700

    added comments to refamiliarize

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,36 @@
-var questionEl = document.querySelector('.question');
+// defined variables
 var questions = ['What do we say to the God of Death?'];
+
+// common selectors
+var questionEl = document.querySelector('.question');
 var startButtonEl = document.querySelector('.start-button-div');
 var questionEl = document.querySelector('.question');
 var optionEl = document.querySelector('.options');
 
+
+// clears starting header and start button
 function clearStart() {
     startButtonEl.innerHTML = '';
     questionEl.textContent = '';
 };
 
+// currently assigns the question element with the question index 0 from questions array
 function inputQuestion() {
     questionEl.innerHTML = questions[0];
 }
 
+// currently places proper button in options section of inner html
 function inputOption() {
     optionEl.innerHTML = '<li><button>Not Today</button></li>'
 }
 
-startButtonEl.addEventListener('click', function () {
-    clearStart();
-    inputQuestion();
-    inputOption();
-});
+// adds event listener to start button to run all initializing functions
+function init() {
+    startButtonEl.addEventListener('click', function () {
+        clearStart();
+        inputQuestion();
+        inputOption();
+    });
+}
+
+init();
