0acb22bed164a862ded7b9dc510d94cf9f2a9821
Maxwell Dunn <max.dunn63@gmail.com>
2021-06-20 15:51:58 -0700

    added ability to cycle through questions with random starting point, new method for generating starting index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 // defined variables
-var questions = ['What do we say to the God of Death?', 'Where are they?!'];
-var answers = ['Not Today', 'He is at 250 52nd street, and she is on Avenue X, on Cisero'];
-startingIndex = Math.floor(Math.random()*questions.length);
-currentIndex = startingIndex
+var questions = ['What do we say to the God of Death?', 'Where are they?!', "Why are you helping me? This isn't your problem."];
+var answers = ['Not Today', 'He is at 250 52nd street, and she is on Avenue X, on Cisero', "My friend's problems are my problems."];
+var wrongAnswer1 = []
+var wrongAnswer2 = []
+var wrongAnswer3 = []
+var startingIndex = genStartIndex();
+var currentIndex = startingIndex;
 
 // common selectors
 var questionEl = document.querySelector('.question');
 var startButtonEl = document.querySelector('.start-button-div');
 var questionEl = document.querySelector('.question');
 var optionEl = document.querySelector('.options');
+
+// generates a random starting index
+
+function genStartIndex() {
+    var randIndex = Math.floor(Math.random() * questions.length)
+    if(randIndex === questions.length) {
+        randIndex = 0
+    }
+    return randIndex;
+}
 
 
 // clears starting header and start button
@@ -19,23 +32,44 @@ function clearStart() {
 
 // currently assigns the question element with the question index 0 from questions array
 function inputQuestion() {
-    if(currentIndex == questions.length) {
-        currentIndex = 0
-    }
     questionEl.innerHTML = questions[currentIndex];
 }
 
 // currently places proper button in options section of inner html
 function inputOption() {
-    optionEl.innerHTML = '<li><button>' + answers[currentIndex] + '</button></li>'
+    optionEl.innerHTML = '<li><button data-state="correct">' + answers[currentIndex] + '</button></li>'
 }
 
 function nextQuestion() {
-    currentIndex = currentIndex++
-    if(currentIndex === questions.length) {
-        currentIndex = 0
+    currentIndex = currentIndex + 1
+    if(currentIndex === questions.length){
+        currentIndex = 0;
+    }
+    if(currentIndex === startingIndex) {
+        // stop timer, save score, go to highlight screen
+        console.log('finished');
+        return
+    }else {
+        inputQuestion();
+        inputOption();
     }
 }
+
+optionEl.addEventListener('click', function (event) {
+    var element = event.target;
+    if(element.matches('li')) {
+        return
+    };
+    if(element.matches('button')) {
+        var userAnswer = element.getAttribute('data-state')
+        console.log(userAnswer);
+        if(userAnswer === 'correct') {
+            nextQuestion();
+            // add success message
+        }
+    }
+
+});
 
 // adds event listener to start button to run all initializing functions
 function init() {
